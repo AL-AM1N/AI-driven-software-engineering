5fc511efd2c2a4cf631cd5102e7cc2921989a2e5
MD. AL-AMIN <amin57.cse@gmail.com>
2026-05-01 01:11:47 +0600

    add typeScript object concept: literal type and access modifier type

--- Typescript-learning/src/non-primitive.ts
+++ Typescript-learning/src/non-primitive.ts
@@ -20,3 +20,48 @@ let nameAndRoll: [string, number] = ["al-amin", 25];
 
 // 3 element touple:
 let destination: [string, string, number] = ["dhaka", "chitagong", 5];
+
+// -----------------------------------------------------------------
+
+//? Object example:
+
+
+// basic object with optional funtionality
+const user: {
+    firstName: string;
+    middleName?: string; //! the middle name is optional by "?"
+    lastName: string;
+    isMarried: boolean;
+} = {
+    firstName: "mohammad",
+    lastName: "al-amin",
+    isMarried: false
+}
+
+
+//* object with "literal types"
+const user1: {
+    organization: "Programming Hero"; //! value => type : literal types (the value is fiexd, it can't be changed)
+    firstName: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    organization: "Programming Hero",
+    firstName: "mohammad",
+    lastName: "al-amin",
+    isMarried: false
+}
+
+
+//* object with "access modifier"
+const user2: {
+    readonly organization: string; //! we can call it "access modifier", bacause we can't change the value of organization
+    firstName: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    organization: "Programming Hero",
+    firstName: "mohammad",
+    lastName: "al-amin",
+    isMarried: false
+}
