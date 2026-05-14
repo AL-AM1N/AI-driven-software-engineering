6f6e1140104ba89f214361cf66396261c179376a
MD. AL-AMIN <amin57.cse@gmail.com>
2026-05-15 01:44:46 +0600

    feat: setup express js, typescript & connect server to neonDB, perform get and post operations using sql command

---
+++ mission2-Be_A_Node_Express_Expert/module_7_expressjs/src/server.ts
@@ -0,0 +1,103 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+
+const app: Application = express();
+const port = 5000;
+
+// middleware: (amra middleware use kori jate server a client theke data ta ashe)
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_ZAPsY2CW7Qjk@ep-dawn-night-aqpclnuw.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require",
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) UNIQUE NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true, 
+            age INT,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+  //res.send("Hello world!")
+  res.status(200).json({
+    message: "Express server",
+    author: "al amin",
+  });
+});
+
+app.post("/api/users", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
+
+  try {
+    const result = await pool.query(
+      `
+    INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4)
+    RETURNING *
+    `,
+      [name, email, password, age],
+    );
+
+    // console.log(result);
+
+    res.status(201).json({
+        message: "User Created successfully!",
+        data: result.rows[0],
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+app.get("/api/users", async(req: Request, res: Response) => {
+    try {
+
+        const result = await pool.query(`
+           SELECT * FROM users  
+            `);
+            res.status(200).json({
+                success: true,
+                message: "Users retrived successfully!",
+                data: result.rows,
+            })
+        
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error,
+        })
+        
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
