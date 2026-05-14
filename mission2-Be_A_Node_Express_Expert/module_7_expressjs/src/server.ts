6fe11bc3366dd6740cd21c0feadf088a4407097f
MD. AL-AMIN <amin57.cse@gmail.com>
2026-05-15 01:58:06 +0600

    Add: get single user end point

--- mission2-Be_A_Node_Express_Expert/module_7_expressjs/src/server.ts
+++ mission2-Be_A_Node_Express_Expert/module_7_expressjs/src/server.ts
@@ -64,9 +64,9 @@ app.post("/api/users", async (req: Request, res: Response) => {
     // console.log(result);
 
     res.status(201).json({
-        message: "User Created successfully!",
-        data: result.rows[0],
-    })
+      message: "User Created successfully!",
+      data: result.rows[0],
+    });
   } catch (error: any) {
     res.status(500).json({
       success: false,
@@ -76,27 +76,58 @@ app.post("/api/users", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/api/users", async(req: Request, res: Response) => {
-    try {
-
-        const result = await pool.query(`
+app.get("/api/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
            SELECT * FROM users  
             `);
-            res.status(200).json({
-                success: true,
-                message: "Users retrived successfully!",
-                data: result.rows,
-            })
-        
-    } catch (error: any) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-            error: error,
-        })
-        
+    res.status(200).json({
+      success: true,
+      message: "Users retrived successfully!",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+       SELECT * FROM users WHERE id=$1  
+        `,
+      [id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+        data: {},
+      });
     }
-})
+
+    res.status(200).json({
+      success: true,
+      message: "User retrived successfully!",
+      data: result.rows[0],
+    });
+    
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
