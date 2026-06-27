dc5ac412556875c249726d78c0cd7566bb71da06
MD. AL-AMIN <amin57.cse@gmail.com>
2026-06-27 12:06:11 +0600

    Rename folder name and create Higher-Order Functions (Wrapper Pattern) for making the code clean

---
+++ mission4-Be_A_Prisma_Pro_and_NextJs_Ninja/project-prisma-press-backend/src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import config from "./config";
+import { userRoutes } from "./modules/user/user.route";
+
+const app: Application = express();
+
+// middleware
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser())
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.use("/api/users", userRoutes);
+
+export default app;
