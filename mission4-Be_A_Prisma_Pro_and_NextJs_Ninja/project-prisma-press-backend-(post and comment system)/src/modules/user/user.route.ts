903305a0320cb69c72ae2d6768d78de1e336c9fe
MD. AL-AMIN <amin57.cse@gmail.com>
2026-06-28 19:42:28 +0600

    Initial commit (previous jwt Authentication part)

---
+++ mission4-Be_A_Prisma_Pro_and_NextJs_Ninja/project-prisma-press-backend-(post and comment system)/src/modules/user/user.route.ts
@@ -0,0 +1,48 @@
+import { Router } from "express";
+import { userController } from "./user.controller";
+import { auth } from "../../middlewares/auth";
+import { Role } from "../../../generated/prisma/enums";
+
+const router = Router();
+
+router.post("/register", userController.registerUser);
+router.get("/me",
+ //! ei part tuku auth middle ware a likha hoyeche   
+//     (req: Request, res: Response, next : NextFunction) => {
+//     console.log(req.cookies);
+//     const {accessToken} = req.cookies;
+//         console.log(accessToken);
+    
+//         const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret);
+
+//         if(!verifiedToken.success){
+//             throw new Error(verifiedToken.error);
+//         }
+  
+//     const { email, name, id, role } = verifiedToken.data as JwtPayload;
+
+//     // const requiredRoles = ["ADMIN", "USER", "AUTHOR"];
+//     const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+//     if(!requiredRoles.includes(role)){
+//         return res.status(403).json({
+//             success: false,
+//             statusCode: httpStatus.FORBIDDEN,
+//             message: "Forbidden. You don't have permission to access this resource."
+//         })
+//     }
+
+//     req.user = {
+//         email,
+//         name,
+//         id,
+//         role
+//     };
+//     next();
+// }, 
+
+auth(Role.ADMIN, Role.USER, Role.AUTHOR), userController.getMyProfile);
+
+router.put("/my-profile", auth(Role.ADMIN, Role.USER, Role.AUTHOR), userController.updateMyProfile)
+
+export const userRoutes = router;
